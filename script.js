bf4a7dfe1f61b50c376bba4d1b1eeb298cd3ce63
anshika-shreshth <anshika.shreshth18@gmail.com>
2024-06-18 08:05:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+//https://api.openweathermap.org/data/2.5/weather?q={Patna}&appid={1648fcbffdc51a10649ed9064edd6b7c}
+
+const apikey= "1648fcbffdc51a10649ed9064edd6b7c";
+const apiurl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchbox =document.querySelector(".search input");
+const searchbtn= document.querySelector(".search button");
+const weathericon= document.querySelector(".weather-icon");
+
+ async function checkweather(city){
+    const response = await fetch (apiurl+city+ `&appid=${apikey}`);
+    var data= await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=data.main.temp+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+"km/hr";
+
+    if(data.weather[0].main== "Clouds"){
+      weathericon.src="clouds.png";
+
+    }
+    else if(data.weather[0].main== "Clear"){
+      weathericon.src="clear.png";
+
+    }
+    else  if(data.weather[0].main== "Rain"){
+      weathericon.src="rain.png";
+
+    }
+    else if(data.weather[0].main== "Drizzle"){
+      weathericon.src="drizzle.png";
+
+    }
+    else if(data.weather[0].main== "Mist"){
+      weathericon.src="mist.png";
+
+
+    }
+    document.querySelector(".weather").style.display="block";
+    searchbox.value = "";
+
+   
+
+ }
+
+ searchbtn.addEventListener("click",()=>{
+   checkweather(searchbox.value);
+
+ })
+
+ //checkweather()
